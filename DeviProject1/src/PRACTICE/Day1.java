542fdfdae847285bf1f7c77999f28a0c4104b040
Devi123-test <sheeladevi2104@gmail.com>
2024-06-26 21:15:44 +0100

    Update using maven integration

--- DeviProject1/src/PRACTICE/Day1.java
+++ DeviProject1/src/PRACTICE/Day1.java
@@ -3,6 +3,7 @@ package PRACTICE;
 public class Day1 {
     public static void main(String[] args) {
         System.out.println("My first output");
+        System.out.println("My Second output using maven integration");
     }
 
 }
